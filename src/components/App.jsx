e1ff0cb69323233c63ccac07020f5dfbd5a87792
EduardOshurkov <drd2993@gmail.com>
2022-07-27 23:17:45 +0300

    deploy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import Feedback from "./Feedback/FeedbackBlock"
 export const App = () => {
   return (
     <div>
-      <Feedback good={0} neutral={0} bad={0}  />
+      <Feedback />
     </div>
   );
 };
